0d9aa1b388bd31bd3914537cb13ac31a4f688b60
akramghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-06-05 12:53:57 +0300

    Create API Routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\BlogController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::apiResource('blogs', BlogController::class);
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    })->name('user');
+});
